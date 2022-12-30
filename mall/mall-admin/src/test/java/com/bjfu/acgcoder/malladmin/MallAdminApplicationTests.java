57443e78015379d690a785ccb5accf833e56bc21
任昊宇 <2001renhaoyu@gmail.com>
2022-12-30 11:52:10 +0800

    Initial commit

---
+++ mall/mall-admin/src/test/java/com/bjfu/acgcoder/malladmin/MallAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bjfu.acgcoder.malladmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
